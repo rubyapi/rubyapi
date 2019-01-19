eb7cd2d62581e3b045f50398001e328fc3a3c752
Colby Swandale <me@colby.fyi>
2019-01-19 12:27:55 +1100

    add standard gem for linting

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,6 @@
-ENV['RAILS_ENV'] ||= 'test'
-require_relative '../config/environment'
-require 'rails/test_help'
+ENV["RAILS_ENV"] ||= "test"
+require_relative "../config/environment"
+require "rails/test_help"
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
