29a4827e19fee80894d6af627f92bc518df0b3ec
Colby Swandale <me@colby.fyi>
2019-01-19 13:57:31 +1100

    add default ruby version config

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module RubyApi
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # default ruby version documentation
+    config.default_ruby_version = '2.6'
   end
 end
