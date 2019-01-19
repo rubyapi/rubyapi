4a4b56e53391acac88b98acb5869815dec7bdbdc
Colby Swandale <me@colby.fyi>
2019-01-19 14:16:31 +1100

    add enums for object/method type

--- app/models/ruby_method.rb
+++ app/models/ruby_method.rb
@@ -1,5 +1,7 @@
 class RubyMethod < ApplicationRecord
 
+  enum method_type: %i(instance_method class_method)
+
   validates :name, :method_type, :version, presence: true
 
   belongs_to :ruby_object
--- app/models/ruby_object.rb
+++ app/models/ruby_object.rb
@@ -1,6 +1,8 @@
 class RubyObject < ApplicationRecord
   include PathGenerator
 
+  enum object_type: %i(module_object class_object)
+
   validates :name, :constant, :object_type, :version, presence: true
 
   belongs_to :ruby_methods
