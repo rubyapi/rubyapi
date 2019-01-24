655a9c45bff1c9c0d5c83ea94c21979936ed66b0
Colby Swandale <me@colby.fyi>
2019-01-24 22:46:48 +1100

    add searchkick to RubyMethod and RubyObject

--- app/models/ruby_object.rb
+++ app/models/ruby_object.rb
@@ -1,6 +1,8 @@
 class RubyObject < ApplicationRecord
   include PathGenerator
 
+  searchkick searchable: [:name, :description], filterable: [:version]
+
   enum object_type: %i[module_object class_object]
 
   validates :name, :constant, :object_type, :version, presence: true
