d3e2b0dda75826eb7f31e13a0c1a8a79d5005dee
Colby Swandale <me@colby.fyi>
2019-01-25 10:21:24 +1100

    add search functionality

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,5 +6,11 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
+  RubyObject.reindex
+  RubyMethod.reindex
+
+  # and disable callbacks
+  Searchkick.disable_callbacks
+
   # Add more helper methods to be used by all tests here...
 end
