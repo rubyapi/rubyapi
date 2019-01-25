dff6ebe3f25d2fe61d6e4fa32aef3ae05b40d68a
Colby Swandale <me@colby.fyi>
2019-01-25 12:18:26 +1100

    fix coveralls not starting

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,7 @@ require_relative "../config/environment"
 require "rails/test_help"
 
 require 'coveralls'
-Coveralls.wear
+Coveralls.wear!
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
