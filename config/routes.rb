b684850d954c26250f7a26c0b2c7277b58bd3c69
Colby Swandale <me@colby.fyi>
2019-01-26 20:07:11 +1100

    add homepage/search routes for ruby versions

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #
-  root to: 'home#index'
+  root to: "home#index"
 
   ruby_versions = Rails.configuration.ruby_versions.collect { |v| Regexp.escape(v) }
 
   scope "(:version)", constraints: { version: /#{ruby_versions.join("|")}/ } do
-    get 's', to: 'search#index', as: :search
-    get 'o/*object', to: 'objects#show', as: :object
+    root to: "home#index"
+    get "s", to: "search#index", as: :search
+    get "o/*object", to: "objects#show", as: :object
   end
 end
