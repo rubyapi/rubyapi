cc108ad49f3d54256e7727665384c7f74f2b04c6
Colby Swandale <me@colby.fyi>
2019-01-27 10:59:35 +1100

    Add tippy.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,5 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+import tippy from 'tippy.js'
+import 'tippy.js/dist/tippy.css'
