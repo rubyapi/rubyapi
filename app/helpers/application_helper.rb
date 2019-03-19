cf9207d0907bf502ffb812ffe5993d438b4a3b25
Colby Swandale <me@colby.fyi>
2019-03-19 22:37:41 +1100

    make standardrb happy

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def homepage?
-    current_page? root_path || current_page?(versioned_root_path(version: ruby_version))
+    current_page?(root_path) || current_page?(versioned_root_path(version: ruby_version))
   end
 end
