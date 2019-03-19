7c8b1257d1819bfeffc19285eea6d766c2ef7e33
Colby Swandale <me@colby.fyi>
2019-03-19 14:12:58 +1100

    done run coveralls when running tests in dev

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,10 +2,12 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
-require "coveralls"
-require "simplecov"
-SimpleCov.add_filter ["lib", "test"]
-Coveralls.wear! "rails"
+if ENV["CI"].present?
+  require "coveralls"
+  require "simplecov"
+  SimpleCov.add_filter ["lib", "test"]
+  Coveralls.wear! "rails"
+end
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
