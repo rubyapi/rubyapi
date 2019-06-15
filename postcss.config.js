fcfe8485aaf46be67e9a773517f016a886b9d735
Colby Swandale <me@colby.fyi>
2019-06-16 00:21:55 +1000

    add postcss-purgecss plugin to help reduce the size of CSS files

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,21 @@
-module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('postcss-flexbugs-fixes'),
-    require('postcss-preset-env')({
-      autoprefixer: {
-        flexbox: 'no-2009'
-      },
-      stage: 3
+let plugins = [
+  require('postcss-import'),
+  require('postcss-flexbugs-fixes'),
+  require('postcss-preset-env')({
+    autoprefixer: {
+      flexbox: 'no-2009'
+    },
+    stage: 3
+  }),
+]
+
+if (process.env.RAILS_ENV === "production") {
+  plugins.push(
+    require('@fullhuman/postcss-purgecss')({
+      content: ['./app/**/*.html.erb'],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
     })
-  ]
+  )
 }
+
+module.exports = { plugins }
