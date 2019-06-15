f20b923f15f74d379f7c4e88d7c51a5f84ec99e9
Colby Swandale <me@colby.fyi>
2019-06-15 23:42:12 +1000

    add keyboard shortcut '/' for focusing on the search

--- app/javascript/packs/app/index.js
+++ app/javascript/packs/app/index.js
@@ -2,3 +2,12 @@ import "./application.css"
 import "@fortawesome/fontawesome-free/css/solid"
 import "@fortawesome/fontawesome-free/css/fontawesome"
 import "@fortawesome/fontawesome-free/css/brands"
+import hotkeys from 'hotkeys-js'
+
+
+hotkeys('/', (event, handler) => {
+  event.preventDefault()
+
+  const search = document.getElementById("search")
+  if(search != null) { search.focus() }
+})
