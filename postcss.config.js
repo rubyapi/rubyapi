6e5592a4055cc696ac551b421deef15593d53528
Colby Swandale <me@colby.fyi>
2019-06-22 14:02:43 +1000

    ooops. Forgot to switch purgecss to look at slim template files

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
   content: [
-    './app/**/*.html.erb',
+    './app/**/*.html.slim',
   ],
   // Include any special characters you're using in this regular expression
   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
