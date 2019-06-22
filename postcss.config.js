600f3bf54eab04c4307adeb41f63353ee3596e80
Colby Swandale <me@colby.fyi>
2019-06-22 12:01:02 +1000

    remove unused css with postcss-purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,25 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [
+    './app/**/*.html.erb',
+  ],
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+})
+
 module.exports = {
   plugins: [
     require('postcss-import'),
+    require('tailwindcss'),
+    require('autoprefixer'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
       autoprefixer: {
         flexbox: 'no-2009'
       },
       stage: 3
-    })
+    }),
+    ...process.env.NODE_ENV === 'production'
+      ? [purgecss]
+      : []
   ]
 }
