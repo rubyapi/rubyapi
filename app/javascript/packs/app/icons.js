509096616f4da5f049468ca602a18350d3e773ef
Colby Swandale <me@colby.fyi>
2019-06-25 21:38:14 +1000

    add faLink fontawesome link

--- app/javascript/packs/app/icons.js
+++ app/javascript/packs/app/icons.js
@@ -1,7 +1,7 @@
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
-import { faSearch, faAngleDown } from '@fortawesome/free-solid-svg-icons'
+import { faSearch, faAngleDown, faLink } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons'
 
-library.add(faSearch, faAngleDown, faGithub, faTwitter)
+library.add(faSearch, faAngleDown, faLink, faGithub, faTwitter)
 
 dom.watch()
