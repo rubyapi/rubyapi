41c07f3b35cbdc44a8a9e2a91bb676bb9edae649
Colby Swandale <me@colby.fyi>
2019-07-24 23:31:15 +1000

    add path field rubyObject

--- app/graphql/types/ruby_object_type.rb
+++ app/graphql/types/ruby_object_type.rb
@@ -8,5 +8,11 @@ module Types
     field :constant, String, null: false
     field :version, String, null: false
     field :ruby_methods, [Types::RubyMethodType], null: false
+    field :path, String, null: false
+
+    def path
+      routes = Rails.application.routes.url_helpers
+      routes.object_path version: object.version, object: object
+    end
   end
 end
