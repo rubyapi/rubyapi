8a4ee277cae2326b33cdadbb043c4d7bd8d8b296
Colby Swandale <me@colby.fyi>
2019-09-26 06:42:36 +1000

    fix breaking Search test due to ElasticSearch index missing

--- test/controllers/search_controller_test.rb
+++ test/controllers/search_controller_test.rb
@@ -1,6 +1,10 @@
 require "test_helper"
 
 class SearchControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    create_index_for_version! default_ruby_version
+  end
+
   test "should get index" do
     get search_url params: {q: "test"}
     assert_response :success
