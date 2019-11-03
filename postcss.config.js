cc864efb5461b4a45b4cd920f8623315013793de
Colby Swandale <me@colby.fyi>
2019-11-04 00:30:33 +1100

    finish off initial dark mode

--- postcss.config.js
+++ postcss.config.js
@@ -10,7 +10,6 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
 
 module.exports = {
   plugins: [
-    require('postcss-import'),
     require('tailwindcss'),
     require('autoprefixer'),
     require('postcss-flexbugs-fixes'),
