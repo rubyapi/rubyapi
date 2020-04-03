eb425f6425e4beb00d7684114583d1f10d92d69c
Alexander Popov <alex.wayfer@gmail.com>
2020-04-03 06:58:46 +0300

    Replace Unicode symbols with Font Awesome icons
    Use Moon Regular for Light theme and Filled for Dark.

--- app/javascript/controllers/theme-switch_controller.js
+++ app/javascript/controllers/theme-switch_controller.js
@@ -1,40 +1,32 @@
 import { Controller } from "stimulus"
 
-function setLightMode(target) {
-  target.classList.replace("dark-switch", "light-switch")
-  document.body.dataset.theme = 'light'
-}
-
-function setDarkMode(target) {
-  target.classList.replace("light-switch", "dark-switch")
-  document.body.dataset.theme = 'dark'
+function setDarkMode(value) {
+  if (value == null) value = document.body.dataset.theme == 'light'
+  document.body.dataset.theme = value ? 'dark' : 'light'
 }
 
 export default class extends Controller {
-  static targets = ["switch"]
-
   connect() {
     const osDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches
     const supportsLocalStorage = 'localStorage' in window
 
     if (supportsLocalStorage) {
       const darkMode = localStorage.getItem('rubyapi-darkMode')
+      console.debug(darkMode)
 
-      if (darkMode !== null && darkMode === '1') {
-        setDarkMode(this.switchTarget)
-      } else if (osDarkMode && darkMode === null) {
-        setDarkMode(this.switchTarget)
+      if (
+        (darkMode !== null && darkMode == 'true') ||
+          (osDarkMode && darkMode == null)
+      ) {
+        setDarkMode(true)
       }
     }
   }
 
   toggle() {
-    if (this.switchTarget.classList.contains('light-switch')) {
-      setDarkMode(this.switchTarget)
-      localStorage.setItem('rubyapi-darkMode', '1')
-    } else {
-      setLightMode(this.switchTarget)
-      localStorage.setItem('rubyapi-darkMode', '0')
-    }
+    setDarkMode()
+    localStorage.setItem(
+      'rubyapi-darkMode', document.body.dataset.theme == 'dark'
+    )
   }
 }
--- app/javascript/packs/app/icons.js
+++ app/javascript/packs/app/icons.js
@@ -1,9 +1,8 @@
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
-import { faSearch, faCaretDown, faLink, faFastForward, faFastBackward, faForward, faBackward, faHistory, faCheck } from '@fortawesome/free-solid-svg-icons'
-import { faPlay, faSync, faInfoCircle } from '@fortawesome/free-solid-svg-icons'
-import { faCopy } from '@fortawesome/free-regular-svg-icons'
+import { faSearch, faCaretDown, faLink, faFastForward, faFastBackward, faForward, faBackward, faHistory, faCheck, faPlay, faSync, faInfoCircle, faMoon as faMoonSolid } from '@fortawesome/free-solid-svg-icons'
+import { faCopy, faMoon as faMoonRegular } from '@fortawesome/free-regular-svg-icons'
 import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons'
 
-library.add(faSearch, faCaretDown, faLink, faFastForward, faFastBackward, faForward, faBackward, faGithub, faTwitter, faHistory, faPlay, faCopy, faCheck, faSync, faInfoCircle)
+library.add(faSearch, faCaretDown, faLink, faFastForward, faFastBackward, faForward, faBackward, faGithub, faTwitter, faHistory, faPlay, faCopy, faCheck, faSync, faInfoCircle, faMoonSolid, faMoonRegular)
 
 dom.watch()
