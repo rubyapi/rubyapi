f9860cc312897b65f07ac62911e697678617da0e
Colby Swandale <me@colby.fyi>
2020-07-18 23:03:14 +1000

    handle nil @@spinner in ImportUI

--- lib/import_ui.rb
+++ lib/import_ui.rb
@@ -2,25 +2,27 @@ require "tty-spinner"
 require "pastel"
 
 module ImportUI
+  @@spinner = nil
+
   def self.reset
     @@spinner = nil
   end
 
   def self.start(message)
     @@spinner ||= TTY::Spinner.new(message)
-    @@spinner.auto_spin
+    @@spinner&.auto_spin
   end
 
   def self.finish(message = "")
-    @@spinner.success(message)
+    @@spinner&.success(message)
   end
 
   def self.error(message)
-    @@spinner.error(message)
+    @@spinner&.error(message)
   end
 
   def self.clear_line
-    @@spinner.clear_line
+    @@spinner&.clear_line
   end
 
   def self.info(message)
