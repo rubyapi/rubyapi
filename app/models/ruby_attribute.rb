35c93bbb8d23eb859e9c87163e32794231416c2e
Colby Swandale <me@colby.fyi>
2020-10-15 21:52:12 +1100

    ditch read/write methods for RubyAttribute

--- app/models/ruby_attribute.rb
+++ app/models/ruby_attribute.rb
@@ -19,18 +19,6 @@ class RubyAttribute
     body[:access]
   end
 
-  def read?
-    access == "read" || readwrite?
-  end
-
-  def write?
-    access == "write" || readwrite?
-  end
-
-  def readwrite?
-    access == "readwrite"
-  end
-
   def to_hash
     {
       name: name,
--- test/models/ruby_attribute_test.rb
+++ test/models/ruby_attribute_test.rb
@@ -8,24 +8,6 @@ class RubyAttributeTest < ActiveSupport::TestCase
   test "required attributes" do
     assert_equal @attribute.name, "HELLO_WORLD"
     assert_equal @attribute.description, "<p>Hello World!</p>"
-    assert_equal @attribute.access, "read"
-  end
-
-  test "read?" do
-    assert_equal RubyAttribute.new(access: "write").read?, false
-    assert_equal RubyAttribute.new(access: "read").read?, true
-    assert_equal RubyAttribute.new(access: "readwrite").read?, true
-  end
-
-  test "write?" do
-    assert_equal RubyAttribute.new(access: "write").write?, true
-    assert_equal RubyAttribute.new(access: "read").write?, false
-    assert_equal RubyAttribute.new(access: "readwrite").write?, true
-  end
-
-  test "readwrite?" do
-    assert_equal RubyAttribute.new(access: "write").readwrite?, false
-    assert_equal RubyAttribute.new(access: "read").readwrite?, false
-    assert_equal RubyAttribute.new(access: "readwrite").readwrite?, true
+    assert_equal @attribute.access, "Read"
   end
 end
