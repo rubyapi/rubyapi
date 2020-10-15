0781a100e3d6138b7b80d3bb503520ecef59df1a
Colby Swandale <me@colby.fyi>
2020-10-16 00:22:41 +1100

    fix spec

--- test/models/ruby_attribute_test.rb
+++ test/models/ruby_attribute_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class RubyAttributeTest < ActiveSupport::TestCase
   def setup
-    @attribute = RubyAttribute.new(name: "HELLO_WORLD", description: "<p>Hello World!</p>", access: "read")
+    @attribute = RubyAttribute.new(name: "HELLO_WORLD", description: "<p>Hello World!</p>", access: "Read")
   end
 
   test "required attributes" do
