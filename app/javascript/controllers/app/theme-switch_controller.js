edeba8d19f3006bdb05d3974ef25d735ec390952
shgtkshruch <shgtk.shruch@gmail.com>
2020-12-26 00:53:38 +0900

    Fixed issue with flickering from light theme to dark on page load
    Extract theme codes from defer loaded application script

--- app/javascript/controllers/app/theme-switch_controller.js
+++ app/javascript/controllers/app/theme-switch_controller.js
@@ -1,40 +1,43 @@
 import { Controller } from "stimulus"
 
+export const setTheme = (switchTarget) => {
+  const osDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches
+  const supportsLocalStorage = 'localStorage' in window
+
+  if (supportsLocalStorage) {
+    const darkMode = localStorage.getItem('rubyapi-darkMode')
+
+    if (darkMode !== null && darkMode === '1') {
+      setDarkMode(switchTarget)
+    } else if (osDarkMode && darkMode === null) {
+      setDarkMode(switchTarget)
+    }
+  }
+}
+
+const setDarkMode = (target) => {
+  if (target) target.classList.replace("fa-sun", "fa-moon")
+  document.documentElement.classList.add("mode-dark")
+  localStorage.setItem('rubyapi-darkMode', '1')
+}
+
+const setLightMode = (target) => {
+  if (target) target.classList.replace("fa-moon", "fa-sun")
+  document.documentElement.classList.remove("mode-dark")
+  localStorage.setItem('rubyapi-darkMode', '0')
+}
 export default class extends Controller {
   static targets = ["switch"]
 
   connect() {
-    const osDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches
-    const supportsLocalStorage = 'localStorage' in window
-
-    if (supportsLocalStorage) {
-      const darkMode = localStorage.getItem('rubyapi-darkMode')
-
-      if (darkMode !== null && darkMode === '1') {
-        this.setDarkMode(this.switchTarget)
-      } else if (osDarkMode && darkMode === null) {
-        this.setDarkMode(this.switchTarget)
-      }
-    }
-  }
-
-  setLightMode(target) {
-    target.classList.replace("fa-moon", "fa-sun")
-    document.documentElement.classList.remove("mode-dark")
-    localStorage.setItem('rubyapi-darkMode', '0')
-  }
-
-  setDarkMode(target) {
-    target.classList.replace("fa-sun", "fa-moon")
-    document.documentElement.classList.add("mode-dark")
-    localStorage.setItem('rubyapi-darkMode', '1')
+    setTheme(this.switchTarget)
   }
 
   toggle() {
     if (this.switchTarget.classList.contains('fa-sun')) {
-      this.setDarkMode(this.switchTarget)
+      setDarkMode(this.switchTarget)
     } else {
-      this.setLightMode(this.switchTarget)
+      setLightMode(this.switchTarget)
     }
   }
 }
