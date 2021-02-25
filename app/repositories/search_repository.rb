3f2ddfc68810edb1ff178bde4ee828be5dc366ed
Colby Swandale <me@colby.fyi>
2021-02-25 21:03:31 +1100

    Lower the number of batch items sent to ES in the Search Repository

--- app/repositories/search_repository.rb
+++ app/repositories/search_repository.rb
@@ -88,7 +88,7 @@ class SearchRepository
   end
 
   def bulk_import(records)
-    records.each_slice(900) do |slice|
+    records.each_slice(500) do |slice|
       entries = slice.each_with_object([]) { |o, arr| arr.push(o.to_hash, *o.ruby_methods.map(&:to_hash)) }
       payload = entries.flat_map { |o| [{"index": {}}, o.to_hash] }
       client.bulk(body: payload, index: index_name)
