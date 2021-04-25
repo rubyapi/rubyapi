9791dec8f1655ae880539dec8a48a699ac238a90
Colby Swandale <me@colby.fyi>
2021-04-25 22:47:09 +1000

    Auto reload page in development when update rails template/view

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,12 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
+const chokidar = require('chokidar')
+
+environment.config.devServer.before = (app, server) => {
+  chokidar.watch([
+    'app/views/**/*'
+  ]).on('change', () => server.sockWrite(server.sockets, 'content-changed'))
+}
 
 module.exports = environment.toWebpackConfig()
