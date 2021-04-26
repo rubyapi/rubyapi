637c9aa0ac0782524262b4aa3d13fa749d578b89
Colby Swandale <me@colby.fyi>
2021-04-26 20:31:39 +1000

    Merge devServer hash instead of assigning it directly

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -3,10 +3,14 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const environment = require('./environment')
 const chokidar = require('chokidar')
 
-environment.config.devServer.before = (app, server) => {
-  chokidar.watch([
-    'app/views/**/*'
-  ]).on('change', () => server.sockWrite(server.sockets, 'content-changed'))
-}
+environment.config.merge({
+  devServer: {
+    before: (app, server, compiler) => {
+      chokidar.watch([
+        'app/views/**/*'
+      ]).on('change', () => server.sockWrite(server.sockets, 'content-changed'))
+    }
+  }
+})
 
 module.exports = environment.toWebpackConfig()
