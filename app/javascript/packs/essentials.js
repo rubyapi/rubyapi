5c52111fe8411b5aedcd4e0cd1e3aa6860ed56b1
Colby Swandale <me@colby.fyi>
2021-05-05 22:48:54 +1000

    normalize header styles and show desktop sidebar menu into mobile dropdown menu
    Co-authored-by: Alexander Popov <alex.wayfer@gmail.com>

--- app/javascript/packs/essentials.js
+++ app/javascript/packs/essentials.js
@@ -1,11 +1,11 @@
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
 import { faSearch, faCaretDown, faLink, faFastForward, faFastBackward, faForward, faBackward, faHistory, faCheck, faSun, faMoon } from '@fortawesome/free-solid-svg-icons'
-import { faPlay, faSync, faInfoCircle, faCode, faExternalLinkAlt, faBars } from '@fortawesome/free-solid-svg-icons'
+import { faPlay, faSync, faInfoCircle, faCode, faExternalLinkAlt, faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
 import { faCopy } from '@fortawesome/free-regular-svg-icons'
 import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons'
 import { setTheme } from 'controllers/app/theme-switch_controller'
 
-library.add(faSearch, faCaretDown, faLink, faFastForward, faFastBackward, faForward, faBackward, faGithub, faTwitter, faHistory, faPlay, faCopy, faCheck, faSync, faInfoCircle, faSun, faMoon, faCode, faExternalLinkAlt, faBars)
+library.add(faSearch, faCaretDown, faLink, faFastForward, faFastBackward, faForward, faBackward, faGithub, faTwitter, faHistory, faPlay, faCopy, faCheck, faSync, faInfoCircle, faSun, faMoon, faCode, faExternalLinkAlt, faBars, faTimes)
 
 dom.watch()
 
