45d9aabb59c864131f8e7b922a3797dfd79e477a
Colby Swandale <me@colby.fyi>
2022-05-27 08:24:40 +1000

    set rails env in Github Actions

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,6 +42,11 @@ Rails.application.configure do
   # Tell Active Support which deprecation messages to disallow.
   config.active_support.disallowed_deprecation_warnings = []
 
+  # Raise an error on page load if there are pending migrations.
+  config.active_record.migration_error = :page_load
+
+  # Highlight code that triggered database queries in logs.
+  config.active_record.verbose_query_logs = true
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
