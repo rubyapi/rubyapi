818448f60692c041a19851f4a5a8a44c5ed6345f
Colby Swandale <me@colby.fiy>
2022-06-09 20:01:06 +1000

    Add anyway_config gem

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,10 @@ Bundler.require(*Rails.groups)
 
 module RubyApi
   class Application < Rails::Application
+    # Configure the path for configuration classes that should be used before initialization
+    # NOTE: path should be relative to the project root (Rails.root)
+    # config.anyway_config.autoload_static_config_path = "config/configs"
+    #
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
