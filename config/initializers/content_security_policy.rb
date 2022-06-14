ee763eede3950ff0c7c35fe40ab14952c6a80afb
Colby Swandale <me@colby.fyi>
2022-06-14 22:35:03 +1000

    allow unsafe_inline styles in CSP

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
     policy.img_src     :self, :https, :data
     policy.object_src  :none
     policy.script_src  :self, :https
-    policy.style_src   :self, :https
+    policy.style_src   :self, :https, :unsafe_inline # Needed by Stimulus Reflex
     policy.connect_src :self, :https
     # Specify URI for violation reports
     # policy.report_uri "/csp-violation-report-endpoint"
