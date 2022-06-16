513951ce907194bb3e79c32f09b1d303dc742f6b
Colby Swandale <me@colby.fyi>
2022-06-16 15:28:32 +1000

    Remove stimulus reflex

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,12 +1,9 @@
 import { Application } from "@hotwired/stimulus"
-import StimulusReflex from 'stimulus_reflex'
 
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
-
-StimulusReflex.initialize(application, { isolate: true })
 
 export { application }
