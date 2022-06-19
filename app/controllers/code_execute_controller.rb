2d32488559eb9a00c0ee63fe40a6070f1b7a54a6
Colby Swandale <me@colby.fyi>
2022-06-19 17:49:37 +1000

    Move repl config into an AnywayConfig object

--- app/controllers/code_execute_controller.rb
+++ app/controllers/code_execute_controller.rb
@@ -4,19 +4,15 @@ class CodeExecuteController < ApplicationController
   REPL_PATH = "exec/%{engine}"
 
   def post
-    response = HTTP.headers("x-api-key" => repl_api_key).post(repl_endpoint, body: params[:snippet])
+    response = HTTP.headers("x-api-key" => ReplConfig.api_key).post(repl_endpoint, body: params[:snippet])
     execution_result = JSON.parse(response.body.to_str, symbolize_names: true)
     render json: {output: execution_result[:output], error: execution_result[:error], status: execution_result[:status]}
   end
 
   private
 
-  def repl_api_key
-    Rails.configuration.repl_api_key
-  end
-
   def repl_endpoint
-    URI.join(repl_host, repl_path).to_s
+    URI.join(ReplConfig.url, repl_path).to_s
   end
 
   def repl_path
@@ -25,9 +21,5 @@ class CodeExecuteController < ApplicationController
 
   def engine
     params[:engine].present? ? params[:engine] : "mri"
-  end
-
-  def repl_host
-    Rails.configuration.repl_host
   end
 end
---
+++ config/configs/repl_config.rb
@@ -0,0 +1,3 @@
+class ReplConfig < ApplicationConfig
+  attr_config url: "http://localhost:3001/", api_key: ""
+end
