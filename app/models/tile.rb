8c3decf250c90ddc9ef04b53e9411a5ac797d32a
Petrik <petrik@deheus.net>
2022-07-03 19:17:17 +0200

    WIP expand the home page tiles

--- app/models/tile.rb
+++
@@ -1,9 +0,0 @@
-class Tile
-  attr_reader :name, :description, :classes
-
-  def initialize(name, description, classes)
-    @name = name
-    @description = description
-    @classes = classes
-  end
-end
