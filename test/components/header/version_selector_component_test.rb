200e902881dae2229bb6c8647cfd3acdbba4f0d3
Colby Swandale <me@colby.fyi>
2022-08-10 10:50:09 +1000

    Add test for Version Selector Component

---
+++ test/components/header/version_selector_component_test.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Header::VersionSelectorComponentTest < ViewComponent::TestCase
+  def test_component_renders_current_ruby_version
+
+    render_inline(Header::VersionSelectorComponent.new(current_version: "3.1", versions: []))
+
+    assert has_button? "3.1"
+  end
+
+  def test_component_renders_ruby_versions
+    ruby_versions = [
+      RubyVersion.new("3.1"),
+      RubyVersion.new("2.7"),
+    ]
+
+    render_inline(Header::VersionSelectorComponent.new(current_version: "3.1", versions: ruby_versions))
+
+    assert has_link? "3.1", href: "/3.1"
+    assert has_link? "2.7", href: "/2.7"
+  end
+end
