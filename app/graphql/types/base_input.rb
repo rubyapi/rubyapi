94f3642b0f4084e4fafff3f9408b980f7ad4a23d
Colby Swandale <me@colby.fyi>
2022-09-26 22:11:06 +1000

    Update core GraphQL classes

--- app/graphql/types/base_input.rb
+++ app/graphql/types/base_input.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Types
-  class BaseInputObject < GraphQL::Schema::InputObject
+  class BaseField < GraphQL::Schema::Field
     argument_class Types::BaseArgument
   end
 end
--- app/graphql/types/base_interface.rb
+++ app/graphql/types/base_interface.rb
@@ -3,5 +3,9 @@
 module Types
   module BaseInterface
     include GraphQL::Schema::Interface
+    edge_type_class(Types::BaseEdge)
+    connection_type_class(Types::BaseConnection)
+
+    field_class Types::BaseField
   end
 end
