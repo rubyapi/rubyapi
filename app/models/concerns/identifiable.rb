5e8cebc77771d8175dd8f1116a73432dba5f5d13
Colby Swandale <me@colby.fyi>
2022-09-27 21:24:05 +1000

    Build path on Ruby Objects

--- app/models/concerns/identifiable.rb
+++ app/models/concerns/identifiable.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This concerns encapsulates methods that allows us to build IDs & URL paths for RubyObjects, SuperClasses & Included Modules
 # that can be used in URLs and ElasticSearch
 module Identifiable
@@ -8,8 +10,9 @@ module Identifiable
     attribute :path, Types::String.optional.default(nil) # This field is automatically generated
 
     def initialize(attributes)
-      attributes[:path] = attributes[:constant]&.downcase&.gsub(/::/, "/")
+      attributes[:path] = self.class.path_from_constant(attributes[:constant])
       attributes[:id] = self.class.id_from_path(attributes[:path])
+
       super(attributes)
     end
   end
@@ -17,6 +20,10 @@ module Identifiable
   class_methods do
     def id_from_path(path)
       Base64.strict_encode64(path)
+    end
+
+    def path_from_constant(constant)
+      constant&.downcase&.gsub(/::/, "/")
     end
   end
 end
