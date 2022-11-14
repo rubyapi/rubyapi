8392c06cfb93b7ddf7f3de50df1dbd6d0f7d76d0
Colby Swandale <me@colby.fyi>
2022-11-14 23:03:25 +1100

    Remove Theme Stimulus Controller and essentials.js

--- app/javascript/controllers/app/index.js
+++ app/javascript/controllers/app/index.js
@@ -4,7 +4,6 @@ import GithubLinksController from "./github_links_controller"
 import HeaderController from "./header_controller"
 import MethodController from "./method_controller"
 import SearchController from "./search_controller"
-import ThemeSwitchController from "./theme-switch_controller"
 import Dropdown from 'stimulus-dropdown'
 
 application.register("code-example", CodeExampleController)
@@ -12,5 +11,4 @@ application.register("github-links", GithubLinksController)
 application.register("header", HeaderController)
 application.register("method", MethodController)
 application.register("search", SearchController)
-application.register("theme-switch", ThemeSwitchController)
 application.register("dropdown", Dropdown)
