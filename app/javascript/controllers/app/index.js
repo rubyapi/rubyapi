944d69b131e5f186f5e9d8dfb25ba7d789c2a908
Colby Swandale <me@colby.fyi>
2022-11-17 22:34:36 +1100

    remove unused Github links Stimulus controller

--- app/javascript/controllers/app/index.js
+++ app/javascript/controllers/app/index.js
@@ -1,6 +1,5 @@
 import { application } from "../application"
 import CodeExampleController from "./code_example_controller"
-import GithubLinksController from "./github_links_controller"
 import HeaderController from "./header_controller"
 import MethodController from "./method_controller"
 import SearchController from "./search_controller"
@@ -8,7 +7,6 @@ import ThemeSwitchController from "./theme-switch_controller"
 import Dropdown from 'stimulus-dropdown'
 
 application.register("code-example", CodeExampleController)
-application.register("github-links", GithubLinksController)
 application.register("header", HeaderController)
 application.register("method", MethodController)
 application.register("search", SearchController)
