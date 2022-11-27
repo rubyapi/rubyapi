213fd9299caded958d4851a10521750abeb099fa
Colby Swandale <me@colby.fyi>
2022-11-27 22:42:01 +1100

    Add theme javascript to automatically set light/dark for system theme

---
+++ app/javascript/theme.js
@@ -0,0 +1,14 @@
+
+// Set Dark Mode if the browser prefers it
+if(window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  document.querySelector('html').classList.add('dark')
+}
+
+// Watch for changes to `prefers-color-scheme` media query and update the theme
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (e) => {
+  if (e.matches) {
+    document.querySelector('html').classList.add('dark')
+  } else {
+    document.querySelector('html').classList.remove('dark')
+  }
+})
