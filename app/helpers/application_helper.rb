e6d9b2ff7e56b33ff72728874bd1b848e1229572
Colby Swandale <me@colby.fyi>
2023-01-25 23:30:12 +1100

    fix linting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,7 +10,7 @@ module ApplicationHelper
 
   # Map a method source file into a url to Github.com
   def github_url(ruby_doc)
-    version, file, line = ruby_doc.source_location.split(":")
+    _version, file, line = ruby_doc.source_location.split(":")
 
     # Not using URI.join, for a performance optimization. URI.join does A LOT of allocations.
     # We know that our source_location is safe, because we make it in the importer.
