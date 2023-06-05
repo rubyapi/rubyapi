afdad680b38682ce0e7218760c2a98a0e3a8ce49
Colby Swandale <me@colby.fyi>
2023-06-05 21:06:50 +1000

    add aws config object

---
+++ config/configs/aws_config.rb
@@ -0,0 +1,39 @@
+# fozen_string_literal: true
+
+class AwsConfig < ApplicationConfig
+
+  DEFAULT_AUTHENTICATION_PROVIDER = "static"
+  CREDENTIAL_PROVIDERS = %w[static ecs].freeze
+
+  attr_accessor :credentials
+
+  attr_config :authentication_provider
+  attr_config :access_key_id, :secret_access_key, :session_token, :region
+
+  on_load :set_credentials
+
+  def set_credentials
+    self.credentials = credentials_for_provider(authentication_provider.presence || DEFAULT_AUTHENTICATION_PROVIDER)
+  end
+
+  private
+
+  def credentials_for_provider(provider)
+    case provider
+    when "static"
+      Aws::Credentials.new(self.access_key_id, self.secret_access_key, self.session_token)
+    when "ecs"
+      Aws::ECSCredentials.new(ecs_credential_options)
+    else
+      raise "Invalid AWS credential provider: #{provider}"
+    end
+  end
+
+  def ecs_credential_options
+    if Rails.env.test?
+      { credential_path: "/test/path" }
+    else
+      {}
+    end
+  end
+end
---
+++ test/config/aws_config_test.rb
@@ -0,0 +1,30 @@
+require "test_helper"
+
+class AwsConfigTest < ActiveSupport::TestCase
+  include Anyway::Testing::Helpers
+
+  test "with static aws credentials" do
+    with_env(
+      "AWS_AUTHENTICATION_PROVIDER" => "static",
+      "AWS_ACCESS_KEY_ID" => "<access_key_id>",
+      "AWS_SECRET_ACCESS_KEY" => "<secret_access_key>",
+      "AWS_REGION" => "<region>",
+      "AWS_SESSION_TOKEN" => "<session_token>"
+    ) do
+      credentials = AwsConfig.new.credentials
+      assert_kind_of Aws::Credentials, credentials
+      assert_equal "<access_key_id>", credentials.access_key_id
+      assert_equal "<secret_access_key>", credentials.secret_access_key
+      assert_equal "<session_token>", credentials.session_token
+    end
+  end
+
+  test "with ecs aws credential provider" do
+    with_env(
+      "AWS_AUTHENTICATION_PROVIDER" => "ecs"
+    ) do
+      credentials = AwsConfig.new.credentials
+      assert_kind_of Aws::ECSCredentials, credentials
+    end
+  end
+end
