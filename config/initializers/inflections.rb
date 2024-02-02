ad60756410c92344b1aade9fa05d004d491fe5b5
Colby Swandale <996377+colby-swandale@users.noreply.github.com>
2024-02-02 16:25:41 +1100

    resolve zietwerk not being able to autoload certain classes automatically

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -13,4 +13,7 @@
 # These inflection rules are supported but not enabled by default:
 ActiveSupport::Inflector.inflections(:en) do |inflect|
   inflect.acronym "API"
+  inflect.acronym "UI"
+  inflect.acronym "RDoc"
+  inflect.acronym "RubyAPI"
 end
