233099f375ec07d420f80b59cb2d26ac5da7189e
Colby Swandale <996377+colby-swandale@users.noreply.github.com>
2024-05-30 13:31:33 +1000

    enable lograge formatted logs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,7 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # Enable lograge for JSON-formatted logs.
+  config.lograge.enabled = true
 end
