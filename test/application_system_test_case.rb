b3702b813cf35b56d584cac9103d7ec960c2d89a
Colby Swandale <996377+colby-swandale@users.noreply.github.com>
2025-04-06 05:58:49 +0000

    setup system test for devcontainers and remove theme test

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,5 +3,13 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+  options = ENV["DEVCONTAINER_APP_HOST"].present? ? { browser: :remote, url: ENV["SELENIUM_URL"] } : {}
+  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400], options: options
+end
+
+if ENV["DEVCONTAINER_APP_HOST"].present?
+  Capybara.always_include_port = true
+  Capybara.server_host = "0.0.0.0"
+  Capybara.server_port = ENV["CAPYBARA_SERVER_PORT"]&.to_i
+  Capybara.app_host = "#{ENV['DEVCONTAINER_APP_HOST']}:#{Capybara.server_port}"
 end
