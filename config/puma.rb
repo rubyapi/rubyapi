28764058ed7ee0a65842e7df4b5bd57c844ed98c
Colby Swandale <996377+colby-swandale@users.noreply.github.com>
2025-04-18 10:02:26 +0000

    make solid queue opt-in only in development

--- config/puma.rb
+++ config/puma.rb
@@ -34,7 +34,7 @@ port ENV.fetch("PORT", 3000)
 plugin :tmp_restart
 
 # Run the Solid Queue supervisor inside of Puma for single-server deployments
-plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"] || Rails.env.local?
+plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
 
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
