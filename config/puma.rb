1b97c947352cee528d15845153bd30379d5d9650
Colby Swandale <996377+colby-swandale@users.noreply.github.com>
2025-08-18 15:11:25 +1000

    enable statsd plugin in puma

--- config/puma.rb
+++ config/puma.rb
@@ -33,6 +33,9 @@ port ENV.fetch("PORT", 3000)
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
 
+# Export Puma metrics to Statsd
+plugin :statsd
+
 # Run the Solid Queue supervisor inside of Puma for single-server deployments
 plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
 
