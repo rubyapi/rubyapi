6d3d18d1225d22569be2a252be01fabee3cfedb5
Colby Swandale <996377+colby-swandale@users.noreply.github.com>
2025-09-28 22:29:15 +1000

    Generate RubyObject model

---
+++ db/migrate/20250928114353_create_ruby_objects.rb
@@ -0,0 +1,16 @@
+class CreateRubyObjects < ActiveRecord::Migration[8.0]
+  def change
+    create_table :ruby_objects do |t|
+      t.belongs_to :documentable, polymorphic: true
+      t.string :name, null: false
+      t.string :path, null: false
+      t.string :description
+      t.string :object_type, null: false
+      t.string :constant, null: false
+      t.string :superclass
+      t.string :included_modules, array: true, default: []
+      t.jsonb :metadata, default: {}
+      t.timestamps
+    end
+  end
+end
