2397a35dd2f5212c9eb0b8356129bcad1a3a3402
Colby Swandale <996377+colby-swandale@users.noreply.github.com>
2025-09-29 09:55:11 +1000

    spec out ruby object and ruby method

--- app/models/ruby_object.rb
+++ app/models/ruby_object.rb
@@ -1,2 +1,11 @@
 class RubyObject < ApplicationRecord
+  has_many :ruby_methods, dependent: :destroy
+
+  def class_object?
+    object_type == "class"
+  end
+
+  def module_object?
+    object_type == "module"
+  end
 end
--- test/models/ruby_object_test.rb
+++ test/models/ruby_object_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class RubyObjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "object types" do
+    class_object = RubyObject.new(object_type: "class")
+    module_object = RubyObject.new(object_type: "module")
+
+    assert class_object.class_object?
+    refute class_object.module_object?
+
+    assert module_object.module_object?
+    refute module_object.class_object?
+  end
 end
