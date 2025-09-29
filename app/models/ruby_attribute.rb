71af2f53eeb948cc8aac164f179b4c62bfeee0dc
Colby Swandale <996377+colby-swandale@users.noreply.github.com>
2025-09-29 10:28:40 +1000

    Add ruby attribute model

--- app/models/ruby_attribute.rb
+++ app/models/ruby_attribute.rb
@@ -1,7 +1,4 @@
-# frozen_string_literal: true
-
-class RubyAttribute < Dry::Struct
-  attribute :name, Types::String
-  attribute :description, Types::String
-  attribute :access, Types::String.default("public")
+class RubyAttribute < ApplicationRecord
+  belongs_to :ruby_object
+  validates :name, presence: true
 end
