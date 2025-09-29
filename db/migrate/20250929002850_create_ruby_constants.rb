1fd5df246cfdc80bf185674caead46a1c1a13aef
Colby Swandale <996377+colby-swandale@users.noreply.github.com>
2025-09-29 23:38:30 +1000

    resolve linting

--- db/migrate/20250929002850_create_ruby_constants.rb
+++ db/migrate/20250929002850_create_ruby_constants.rb
@@ -1,7 +1,7 @@
 class CreateRubyConstants < ActiveRecord::Migration[8.0]
   def change
     create_table :ruby_constants do |t|
-      t.belongs_to :ruby_object, index: true, foreign_key: { on_delete: :cascade }
+      t.belongs_to :ruby_object, index: true, foreign_key: {on_delete: :cascade}
       t.string :name, null: false
       t.text :description
       t.string :constant, null: false
