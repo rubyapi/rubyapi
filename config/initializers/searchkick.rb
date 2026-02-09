782d80c99cfd149a522b260c187799913025e1c0
Colby Swandale <996377+colby-swandale@users.noreply.github.com>
2026-02-10 01:21:16 +1100

    Fine tuning for Kamal

--- config/initializers/searchkick.rb
+++ config/initializers/searchkick.rb
@@ -1,1 +1,2 @@
-Searchkick.client_options[:transport_options] =  SearchConfig.transport_options
+ENV["OPENSEARCH_URL"] ||= Rails.application.credentials.opensearch[:url] if Rails.application.credentials.opensearch
+Searchkick.client_options[:transport_options] = SearchConfig.transport_options
